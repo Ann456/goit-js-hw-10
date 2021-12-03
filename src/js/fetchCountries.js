570e512e0d818ba4cec80526561e85024c6b1ee8
Ann456 <popovaa769@gmail.com>
2021-12-03 02:58:32 +0200

    added

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
-const BASE_URL = 'https://restcountries.com/v2';
-function fetchCountries(anyCountryName) {
-  return fetch(`${BASE_URL}/name/${anyCountryName}?fields=name,capital,population,flag,languages`)
+const BASE_URL = 'https://restcountries.com/v2';//https://restcountries.com/#api-endpoints-v3-list-of-codes
+function fetchCountries(all) {
+  return fetch(`${BASE_URL}/name/${all}?fields=name,capital,population,flag,languages`)
     .then(resp => resp.json());
 }
 
